45a9876c85c55ce8d553cb4d2827d125ce4de000
shiv <salamchandani@nigul.cide.es>
2023-10-11 17:35:17 +0200

    first commit

---
+++ ex-json/src/main/java/com/cide/exjson/ExJsonApplication.java
@@ -0,0 +1,15 @@
+package com.cide.exjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExJsonApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ExJsonApplication.class, args);
+		//First commit
+	}
+
+}
---
+++ ex-json/src/test/java/com/cide/exjson/ExJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cide.exjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
